2d7926beea9e9613079190b859b92e622905f76e
Ochui Princewill <ochuiprincewill411@gmail.com>
2020-11-11 16:33:55 +0100

    added chat list

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -42,7 +42,7 @@ function saveMessage(messageText) {
     text: messageText,
     profilePicUrl: getProfilePicUrl(),
     timestamp: firebase.firestore.FieldValue.serverTimestamp()
-  }).catch(function(error) {
+  }).catch(function (error) {
     console.error('Error writing new message to Firebase Database', error);
   });
 }
@@ -51,16 +51,17 @@ function saveMessage(messageText) {
 function loadMessages() {
   // Create the query to load the last 12 messages and listen for new ones.
   var query = firebase.firestore().collection('messages').orderBy('timestamp', 'desc').limit(12);
-  
+
   // Start listening to the query.
-  query.onSnapshot(function(snapshot) {
-    snapshot.docChanges().forEach(function(change) {
+  query.onSnapshot(function (snapshot) {
+    snapshot.docChanges().forEach(function (change) {
       if (change.type === 'removed') {
         deleteMessage(change.doc.id);
       } else {
         var message = change.doc.data();
+        console.log(message)
         displayMessage(change.doc.id, message.timestamp, message.name,
-                      message.text, message.profilePicUrl, message.imageUrl);
+          message.text, message.profilePicUrl, message.imageUrl);
       }
     });
   });
@@ -75,10 +76,10 @@ function saveImageMessage(file) {
     imageUrl: LOADING_IMAGE_URL,
     profilePicUrl: getProfilePicUrl(),
     timestamp: firebase.firestore.FieldValue.serverTimestamp()
-  }).then(function(messageRef) {
+  }).then(function (messageRef) {
     // 2 - Upload the image to Cloud Storage.
     var filePath = firebase.auth().currentUser.uid + '/' + messageRef.id + '/' + file.name;
-    return firebase.storage().ref(filePath).put(file).then(function(fileSnapshot) {
+    return firebase.storage().ref(filePath).put(file).then(function (fileSnapshot) {
       // 3 - Generate a public URL for the file.
       return fileSnapshot.ref.getDownloadURL().then((url) => {
         // 4 - Update the chat message placeholder with the image’s URL.
@@ -88,24 +89,24 @@ function saveImageMessage(file) {
         });
       });
     });
-  }).catch(function(error) {
+  }).catch(function (error) {
     console.error('There was an error uploading a file to Cloud Storage:', error);
   });
 }
 
 // Saves the messaging device token to the datastore.
 function saveMessagingDeviceToken() {
-  firebase.messaging().getToken().then(function(currentToken) {
+  firebase.messaging().getToken().then(function (currentToken) {
     if (currentToken) {
       console.log('Got FCM device token:', currentToken);
       // Saving the Device Token to the datastore.
       firebase.firestore().collection('fcmTokens').doc(currentToken)
-          .set({uid: firebase.auth().currentUser.uid});
+        .set({ uid: firebase.auth().currentUser.uid });
     } else {
       // Need to request permissions to show notifications.
       requestNotificationsPermissions();
     }
-  }).catch(function(error){
+  }).catch(function (error) {
     console.error('Unable to get messaging token.', error);
   });
 }
@@ -113,10 +114,10 @@ function saveMessagingDeviceToken() {
 // Requests permissions to show notifications.
 function requestNotificationsPermissions() {
   console.log('Requesting notifications permission...');
-  firebase.messaging().requestPermission().then(function() {
+  firebase.messaging().requestPermission().then(function () {
     // Notification permission granted.
     saveMessagingDeviceToken();
-  }).catch(function(error) {
+  }).catch(function (error) {
     console.error('Unable to get permission to notify.', error);
   });
 }
@@ -149,7 +150,7 @@ function onMessageFormSubmit(e) {
   e.preventDefault();
   // Check that the user entered a message and is signed in.
   if (messageInputElement.value && checkSignedInWithMessage()) {
-    saveMessage(messageInputElement.value).then(function() {
+    saveMessage(messageInputElement.value).then(function () {
       // Clear message text field and re-enable the SEND button.
       resetMaterialTextfield(messageInputElement);
       toggleButton();
@@ -213,11 +214,15 @@ function resetMaterialTextfield(element) {
 
 // Template for messages.
 var MESSAGE_TEMPLATE =
-    '<div class="message-container">' +
-      '<div class="spacing"><div class="pic"></div></div>' +
-      '<div class="message"></div>' +
-      '<div class="name"></div>' +
-    '</div>';
+  '<div class="message-container">' +
+      '<div class="text-center">'+
+          '<img class="pic" src="" style="height : 25px; border-radius:50%; margin-right:10px;" alt="" />'+
+      '</div>'+
+    '<div>'+
+      '<p class="message"></p>' +
+      '<p class="name"></p>' +
+    '</div>'+
+  '</div>';
 
 // Adds a size to Google Profile pics URLs.
 function addSizeToGoogleProfilePic(url) {
@@ -285,7 +290,7 @@ function displayMessage(id, timestamp, name, text, picUrl, imageUrl) {
 
   // profile picture
   if (picUrl) {
-    div.querySelector('.pic').style.backgroundImage = 'url(' + addSizeToGoogleProfilePic(picUrl) + ')';
+    div.querySelector('.pic').src = addSizeToGoogleProfilePic(picUrl);
   }
 
   div.querySelector('.name').textContent = name;
@@ -297,7 +302,7 @@ function displayMessage(id, timestamp, name, text, picUrl, imageUrl) {
     messageElement.innerHTML = messageElement.innerHTML.replace(/\n/g, '<br>');
   } else if (imageUrl) { // If the message is an image.
     var image = document.createElement('img');
-    image.addEventListener('load', function() {
+    image.addEventListener('load', function () {
       messageListElement.scrollTop = messageListElement.scrollHeight;
     });
     image.src = imageUrl + '&' + new Date().getTime();
@@ -305,7 +310,7 @@ function displayMessage(id, timestamp, name, text, picUrl, imageUrl) {
     messageElement.appendChild(image);
   }
   // Show the card fading-in and scroll to view the new message.
-  setTimeout(function() {div.classList.add('visible')}, 1);
+  setTimeout(function () { div.classList.add('visible') }, 1);
   messageListElement.scrollTop = messageListElement.scrollHeight;
   messageInputElement.focus();
 }
@@ -324,8 +329,8 @@ function toggleButton() {
 function checkSetup() {
   if (!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
     window.alert('You have not configured and imported the Firebase SDK. ' +
-        'Make sure you go through the codelab setup instructions and make ' +
-        'sure you are running the codelab using `firebase serve`');
+      'Make sure you go through the codelab setup instructions and make ' +
+      'sure you are running the codelab using `firebase serve`');
   }
 }
 
@@ -356,7 +361,7 @@ messageInputElement.addEventListener('keyup', toggleButton);
 messageInputElement.addEventListener('change', toggleButton);
 
 // Events for image upload.
-imageButtonElement.addEventListener('click', function(e) {
+imageButtonElement.addEventListener('click', function (e) {
   e.preventDefault();
   mediaCaptureElement.click();
 });
@@ -365,8 +370,24 @@ mediaCaptureElement.addEventListener('change', onMediaFileSelected);
 // initialize Firebase
 initFirebaseAuth();
 
- // TODO: Enable Firebase Performance Monitoring.
+// TODO: Enable Firebase Performance Monitoring.
 firebase.performance();
 
 // We load currently existing chat messages and listen to new ones.
 loadMessages();
+
+function login() {
+  document.getElementById('modal_signup').style.display = 'block';
+}
+
+function signup() {
+  document.getElementById('modal_signin').style.display = 'block';
+}
+
+function sub() {
+  document.getElementById('modal_sub').style.display = "block";
+}
+
+function cross() {
+  document.getElementsByClassName('modall')[0].style.display = 'none';
+}
